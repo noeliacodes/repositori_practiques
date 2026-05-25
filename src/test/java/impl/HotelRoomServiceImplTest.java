0c49f730a074f4414d5f44239b51bf0652537f56
Noelia Sanchis Blay <noesanbla2@alu.edu.gva.es>
2026-05-25 12:30:41 +0200

    inici del segon intent amb branques

---
+++ src/test/java/impl/HotelRoomServiceImplTest.java
@@ -0,0 +1,22 @@
+package impl;
+
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import ud8.common.exception.ResourceNotFoundException;
+import ud8.hotel.domain.entity.HotelRoom;
+import ud8.hotel.domain.service.NotificationService;
+import ud8.hotel.domain.service.impl.HotelRoomServiceImpl;
+import ud8.hotel.persistance.repository.HotelRoomRepository;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+
+class HotelRoomServiceImplTest {
+
+}
